b52c1dbc2b153f303b9006cef7000130581db6af
NadishiChristina <nadishiramanayake@gmail.com>
2025-06-01 00:31:45 +0530

    Add loader

---
+++ src/components/UI/LoadingSpinner.jsx
@@ -0,0 +1,9 @@
+const LoadingSpinner = () => {
+  return (
+    <div className="min-h-screen bg-gray-900 flex items-center justify-center">
+      <div className="text-white text-xl">Loading weather data...</div>
+    </div>
+  );
+};
+
+export default LoadingSpinner;
