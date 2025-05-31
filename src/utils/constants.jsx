26eee5709c0f3e10c751c5150b25771e774e4eb6
NadishiChristina <nadishiramanayake@gmail.com>
2025-05-31 23:58:03 +0530

    Add city data

---
+++ src/utils/constants.jsx
@@ -0,0 +1,23 @@
+export const CITIES_DATA = {
+  "List": [
+    {"CityCode": "1248991", "CityName": "Colombo", "Temp": "33.0", "Status": "Clouds"},
+    {"CityCode": "1850147", "CityName": "Tokyo", "Temp": "8.6", "Status": "Clear"},
+    {"CityCode": "2644210", "CityName": "Liverpool", "Temp": "16.5", "Status": "Rain"},
+    {"CityCode": "2988507", "CityName": "Paris", "Temp": "22.4", "Status": "Clear"},
+    {"CityCode": "2147714", "CityName": "Sydney", "Temp": "27.3", "Status": "Rain"},
+    {"CityCode": "4930956", "CityName": "Boston", "Temp": "4.2", "Status": "Mist"},
+    {"CityCode": "1796236", "CityName": "Shanghai", "Temp": "10.1", "Status": "Clouds"},
+    {"CityCode": "3143244", "CityName": "Oslo", "Temp": "-3.9", "Status": "Clear"}
+  ]
+};
+
+export const WEATHER_CARD_COLORS = [
+  'from-blue-500 to-blue-600',
+  'from-purple-500 to-purple-600', 
+  'from-green-500 to-green-600',
+  'from-orange-500 to-orange-600',
+  'from-red-500 to-red-600',
+  'from-teal-500 to-teal-600',
+  'from-indigo-500 to-indigo-600',
+  'from-pink-500 to-pink-600'
+];
