0d9dbfb35a49a31b3f7af036ca1ecc578112d4ac
NadishiChristina <nadishiramanayake@gmail.com>
2025-06-02 21:35:35 +0530

    Modify loader

--- src/components/UI/LoadingSpinner.jsx
+++ src/components/UI/LoadingSpinner.jsx
@@ -3,14 +3,16 @@ import './LoadingSpinner.css';
 
 const LoadingSpinner = () => {
   return (
-    // spinner - wind animation 
+    // spinner - wind animation
     <div className="min-h-screen bg-blue-950 flex flex-col items-center justify-center">
-      <div className="wind"> 
+      <div className="wind">
         <span></span>
         <span></span>
         <span></span>
       </div>
-      <p className="text-white text-lg mt-6">LOOKING OUTSIDE FOR YOU...ONE SEC</p>
+      <p className="text-white text-xl mt-8 font-semibold tracking-wide animate-pulse">
+        LOOKING OUTSIDE FOR YOU...ONE SEC
+      </p>
     </div>
   );
 };
